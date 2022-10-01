007f3ba42104bf76cdbea164920818a28c339bd6
Cameron Sabiston <camgsab99@live.com>
2022-10-01 19:45:05 -0400

    HW3

--- Homework 3/hwk3.hs
+++ Homework 3/hwk3.hs
@@ -1,3 +1,7 @@
+-- Cameron Sabiston
+-- CSCI 3675
+-- 10/01/2022
+import Data.Char
 -- Returns the square of each odd number in the list
 squareOddsRecursive :: [Int] -> [Int]
 squareOddsRecursive [] = []
@@ -9,11 +13,11 @@ squareOddsComprension :: [Int] -> [Int]
 squareOddsComprension xs = [x^2 | x <- xs, odd x]
 
 -- Returns the string with the first letter capitalized using list comprehension
---capitalize :: String -> String
---capitalize (x:xs) = [toUpper x] ++ xs
+capitalizeComprehension :: String -> String
+capitalizeComprehension xs = [toUpper x | x <- xs, x == head xs] ++ [toLower x | x <- xs, x /= head xs]
 
--- Return the product of all the elements in the list
-prodRecursive :: [Int] -> Int
+-- Return the product of all the elements in the list recursively
+prodRecursive :: Num a => [a] -> a
 prodRecursive [] = 1
 prodRecursive (x:xs) = x * prodRecursive xs
 
@@ -29,23 +33,23 @@ inRangeRecursive _ _ [] = []
 inRangeRecursive a b (x:xs) = if x >= a && x <= b then x : tail else tail where
     tail = inRangeRecursive a b xs
 
--- returns all numbers in the input list within the range given by the first two arguments using list comprehension
+-- Returns all numbers in the input list within the range given by the first two arguments using list comprehension
 inRangeComprension :: Int -> Int -> [Int] -> [Int]
 inRangeComprension a b xs = [x | x <- xs, x >= a, x <= b]
 
--- returns a^b, where b is a non-negative integer
+-- Returns a^b, where b is a non-negative integer
 power a b = if b == 0 then 1 else a * power a (b-1)
 
 -- Test Cases
-main = do
-    print (squareOddsRecursive [0, 2, 1, 7, 8, 56, 17, 18])
-    print (squareOddsComprension [0, 2, 1, 7, 8, 56, 17, 18])
-    --print (capitalizeComprehension "grEENVIlle")
-    print (prodRecursive [3, 2, 1])
-    -- print (prodRecursive [3.0, 2.0])
-    print (palindromeRecursive "abba")
-    print (palindromeRecursive "abb")
-    print (inRangeRecursive 5 10 [9, 3, 12])
-    print (inRangeComprension 5 10 [9, 3, 12])
-    print (power 2.0 1)
-    print (power 2 3)
+-- main = do
+--     print (squareOddsRecursive [0, 2, 1, 7, 8, 56, 17, 18])
+--     print (squareOddsComprension [0, 2, 1, 7, 8, 56, 17, 18])
+--     print (capitalizeComprehension "grEENVIlle")
+--     print (prodRecursive [3, 2, 1])
+--     print (prodRecursive [3.0, 2.0])
+--     print (palindromeRecursive "abba")
+--     print (palindromeRecursive "abb")
+--     print (inRangeRecursive 5 10 [9, 3, 12])
+--     print (inRangeComprension 5 10 [9, 3, 12])
+--     print (power 2.0 1)
+--     print (power 2 3)
